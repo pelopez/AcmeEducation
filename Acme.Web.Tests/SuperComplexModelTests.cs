0ee873f2f11e00eb9c54aea7fa3f07d7eaad25a4
Pete Lopez <petelopez@gmail.com>
2014-04-10 00:35:53 -0400

    Added Unit Tests

---
+++ Acme.Web.Tests/SuperComplexModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.Web.Models;
+using NUnit.Framework;
+
+namespace Acme.Web.Tests
+{
+    [TestFixture]
+    public class SuperComplexModelTests
+    {
+        [Test]
+        public void FullName_Returns_FirstName_And_LastName_Properly_Spaced()
+        {
+            var model = new SuperComplexModel {FirstName = "Pete", LastName = "Lopez"};
+
+            var expected = "Pete Lopez";
+
+            Assert.AreEqual(expected, model.FullName);
+        }
+    }
+}
---
+++ Acme.Web/Models/SuperComplexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Acme.Web.Models
+{
+    public class SuperComplexModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
+    }
+}
