8ae882ed0d7c96b7b3e58545f11aabaa85219c43
Pete Lopez <petelopez@gmail.com>
2014-04-10 00:57:24 -0400

    Fixed bug

--- Acme.Web/Models/SuperComplexModel.cs
+++ Acme.Web/Models/SuperComplexModel.cs
@@ -9,6 +9,6 @@ namespace Acme.Web.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName { get { return string.Format("{0}{1}", FirstName, LastName); } }
+        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
     }
 }
