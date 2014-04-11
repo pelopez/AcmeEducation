8ecc9fcd274e9d42d9df5b688b166cce4bdd8a20
Pete Lopez <petelopez@gmail.com>
2014-04-10 23:37:29 -0400

    Broke stuff for the demo

--- Acme.Web/Models/SuperComplexModel.cs
+++ Acme.Web/Models/SuperComplexModel.cs
@@ -9,11 +9,11 @@ namespace Acme.Web.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
+        public string FullName { get { return string.Format("{0}{1}", FirstName, LastName); } }
 
         public static int PerformComplexAddition(int theFirstNumber, int theSecondNumber)
         {
-            return theFirstNumber + theSecondNumber;
+            return theFirstNumber * theSecondNumber;
         }
     }
 }
