390dbcedd86b11af38eca2a0d80fa409f599b3b1
petelopez <petelopez@gmail.com>
2014-04-11 11:49:38 -0400

    Fixed #ae-3

--- Acme.Web/Models/SuperComplexModel.cs
+++ Acme.Web/Models/SuperComplexModel.cs
@@ -5,7 +5,7 @@ namespace Acme.Web.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName { get { return string.Format("{0}{1}", FirstName, LastName); } }
+        public string FullName { get { return string.Format("{0} {1}", FirstName, LastName); } }
 
         public static int PerformComplexAddition(int theFirstNumber, int theSecondNumber)
         {
